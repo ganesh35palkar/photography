59694f3ed3b077543a4da714a59d48a42abce6e6
Ganesh Palkar <ganesh@Ganeshs-MacBook-Pro.local>
2019-11-09 07:05:07 +0530

    v1

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+//[Custom Javascript]
+
+//Project:	Yoga club - Onepage Html Responsive Template
+//Version:	1.1
+//Primary use:	Yoga club - Onepage Html Responsive Template 
+
+//add your script here
